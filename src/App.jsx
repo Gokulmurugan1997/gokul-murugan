8ff8ce304e0132ff480aceefb3851cec77d0bda4
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-08-21 09:01:06 +0900

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { FaDatabase } from 'react-icons/fa';
+
 
 function App() {
 
@@ -10,7 +13,7 @@ function App() {
                   {'  '}<a href="https://github.com/Gokulmurugan1997" target='_blank'><i className="fa-brands fa-github fa-beat-fade" style={{color: "#f8f8f7"}}></i></a>
               </div><br />
               <div className='resume'>
-                  Resume{' '}<a href="https://drive.google.com/file/d/11nf039gFVGAFqY8fX0HofmDwvKRA13Da/view?usp=drivesdk" target='_blank'><i className="fa-solid fa-download fa-bounce" style={{color: "#f9fbfb"}}></i></a>
+                  Resume{' '}<a href="https://drive.google.com/file/d/12zMA62dJlEF_aOkXBX4w8r2BvozqTSbS/view?usp=drivesdk" target='_blank'><i className="fa-solid fa-download fa-bounce" style={{color: "#f9fbfb"}}></i></a>
               </div>
           </div>
           <div className='container-2'>
@@ -19,7 +22,7 @@ function App() {
               </div>
           <div className='container-3'>
               <h1 className='heading'> Work </h1>
-              <p className='content'><h2>HCLTech – Member Technical Staff - June 2023 to Present</h2><br />
+              <p className='content'><b>HCLTech – Member Technical Staff - June 2023 to Present</b><br />
               Supporting in creation of Project Management Web application called PM SMART for tracking daily and monthly efforts of resource utilization for the project.<br/>
               Supporting in improvisation of Employee Self Service website for maximizing user efficiency
               </p>
@@ -27,80 +30,128 @@ function App() {
           <div className='container-4'>
               <h1>Skills</h1>
           <div>
-          <p>HTML
-              <div class="container-bar">
-              <div class="skills html"></div>
+          HTML
+              <div className="container-bar">
+              <div className="skills html"></div>
             </div>
-          </p>
+         
 
-
-          <p>CSS
-            <div class="container-bar">
-            <div class="skills css"></div>
+          <br />
+          CSS
+            <div className="container-bar">
+            <div className="skills css"></div>
             </div>
-          </p>
-
-          <p>JavaScript
-            <div class="container-bar">
-            <div class="skills js"></div>
+        
+            <br />
+          JavaScript
+            <div className="container-bar">
+            <div className="skills js"></div>
             </div>
-          </p>
-
-          <p>ReactJs
-            <div class="container-bar">
-            <div class="skills react"></div>
+          
+          <br />
+          ReactJs
+            <div className="container-bar">
+            <div className="skills react"></div>
             </div>
-          </p>
       </div>
     <div>
-          <p>NodeJs
-            <div class="container-bar">
-            <div class="skills node"></div>
+          NodeJs
+            <div className="container-bar">
+            <div className="skills node"></div>
             </div>
-          </p>
-          <p>ExpressJs
-            <div class="container-bar">
-            <div class="skills express"></div>
+          <br />
+          ExpressJs
+            <div className="container-bar">
+            <div className="skills express"></div>
             </div>
-          </p>
-          <p>SQL
-            <div class="container-bar">
-            <div class="skills sql"></div>
+          <br />
+          SQL
+            <div className="container-bar">
+            <div className="skills sql"></div>
             </div>
-          </p>
-          <p>MongoDB
-            <div class="container-bar">
-            <div class="skills db"></div>
+          <br />
+          MongoDB
+            <div className="container-bar">
+            <div className="skills db"></div>
             </div>
-          </p>
+          
         </div>
           </div>
+         
           <div className='container-5'>
                 <h1>Projects</h1>
                 <div>
                 <div className='projects'>
-                      HTML Markdown viewer
-                      <a href="https://main--regal-alfajores-20ad80.netlify.app/home" className='fa-beat' target='_blank'>view</a>
+                  <div className='image1'></div>
+                      <a href="https://main--regal-alfajores-20ad80.netlify.app/home" target='_blank'><Button className='button'>HTML Markdown viewer</Button></a>
+                      <div>
+                      <a href="https://github.com/Gokulmurugan1997/Markdown_Viewer-front-end" target='_blank'><Button className='front'>Front</Button></a>
+                      <a href="https://github.com/Gokulmurugan1997/Markdown_Viewer-Back_end" target='_blank'><Button className='back'>Back</Button></a>
+                      </div>
+                      <div className='icons'>
+                      <i className="fa-brands fa-react" style={{color: "#4ccc28"}}></i>{' '}
+                      <i className="fa-brands fa-node-js" style={{color:"#12de45"}}></i>{" "}
+                      <i className="fa-brands fa-js" style={{color: "#FFD43B"}}></i>{' '}
+                      <i className="fa-brands fa-html5" style={{color: "#f20736"}}></i>{' '}
+                      <i className="fa-brands fa-css3-alt" style={{color: "#74C0FC"}}></i>{' '}
+                      <FaDatabase size={20} color="#4DB33D" />
+                      </div>
                 </div>
                 <br /><br /><br /><br /><br />
                 <div className='projects'>
-                  Shopping Cart
-                  <a href="https://main--sweet-kitten-e41d22.netlify.app/" className='fa-beat' target='_blank'>view</a>
+                <div className='image2'></div>
+                  <a href="https://thriving-marigold-a1bc3b.netlify.app/login" target='_blank'><Button className='button'>Shopping Cart</Button></a>
+                  <div>
+                      <a href="https://github.com/Gokulmurugan1997/shop_front" target='_blank'><Button className='front'>Front</Button></a>
+                      <a href="https://github.com/Gokulmurugan1997/shop_back" target='_blank'><Button className='back'>Back</Button></a>
+                      </div>
+                      <div className='icons'>
+                      <i className="fa-brands fa-react" style={{color: "#4ccc28"}}></i>{' '}
+                      <i className="fa-brands fa-node-js" style={{color:"#12de45"}}></i>{" "}
+                      <i className="fa-brands fa-js" style={{color: "#FFD43B"}}></i>{' '}
+                      <i className="fa-brands fa-html5" style={{color: "#f20736"}}></i>{' '}
+                      <i className="fa-brands fa-css3-alt" style={{color: "#74C0FC"}}></i>{' '}
+                      <FaDatabase size={20} color="#4DB33D" />
+                      </div>
                 </div>
                 </div>
                 <div>
                 <div className='projects'>
-                    Users Data App
-                    <a href="https://main--sunny-jalebi-e8ffba.netlify.app/" className='fa-beat' target='_blank'>view</a>
+                    <div className='image3'>
+
+                    </div>
+                    <a href="https://main--sunny-jalebi-e8ffba.netlify.app/" target='_blank'><Button className='button'>Users Data App</Button></a>
+                    <div>
+                      <a href="https://github.com/Gokulmurugan1997/axios_task" target='_blank'><Button className='front'>Front</Button></a>
+                      </div>
+                      <div className='icons'>
+                      <i className="fa-brands fa-react" style={{color: "#4ccc28"}}></i>{' '}
+                      <i className="fa-brands fa-js" style={{color: "#FFD43B"}}></i>{' '}
+                      <i className="fa-brands fa-html5" style={{color: "#f20736"}}></i>{' '}
+                      <i className="fa-brands fa-css3-alt" style={{color: "#74C0FC"}}></i>{' '}
+                      </div>
                 </div>
                 <br /><br /><br /><br /><br />
                 <div className='projects'>
-                    Dashboard Page
-                    <a href="https://courageous-cassata-f1bdef.netlify.app/dashboard" className='fa-beat' target='_blank'>view</a>
+                <div className='image4'>
+
                 </div>
+                    <a href="https://courageous-cassata-f1bdef.netlify.app/dashboard" target='_blank'><Button className='button'>Dashboard Page</Button></a>
+                    <div>
+                      <a href="https://github.com/Gokulmurugan1997/landing_page" target='_blank'><Button className='front'>Front</Button></a>
+                      </div>
+                      <div className='icons'>
+                      <i className="fa-brands fa-react" style={{color: "#4ccc28"}}></i>{' '}
+                      <i className="fa-brands fa-js" style={{color: "#FFD43B"}}></i>{' '}
+                      <i className="fa-brands fa-html5" style={{color: "#f20736"}}></i>{' '}
+                      <i className="fa-brands fa-css3-alt" style={{color: "#74C0FC"}}></i>{' '}
+                      </div>
                 </div>
-          
+
+                </div>
         </div>
+        <div>
+    </div>
   </div>
 }
 
